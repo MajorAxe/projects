2193eb6d0b6c40f8badeea59b7804acc7358ba4b
iamssa <alexandrsimakovv@mail.ru>
2019-02-16 17:55:40 +0500

    add test xsolla
    add index.html and style.css

--- Git/udemy project/landingPage/js/script.js
+++ Git/udemy project/landingPage/js/script.js
@@ -34,4 +34,20 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+    
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+    });
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+    })
+
 });

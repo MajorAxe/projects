c6d0a01f9a1ddd04b17b0fd596e5462e65d4297f
iamssa <alexandrsimakovv@mail.ru>
2019-02-17 15:58:29 +0500

    add functional
    - add main.js
    - change style buttons

---
+++ Git/test xsolla/js/main.js
@@ -0,0 +1,63 @@
+//кнопки с цифрами
+    let btnNumber = document.querySelectorAll(".number"),
+
+//операции
+    equals = document.getElementById("equals"),
+    btnOperations = document.querySelectorAll(".operations"),
+    
+//знаки
+    btnSymbol = document.querySelectorAll(".symbol"),
+
+    result = 0;
+    string = document.querySelector("#string");
+
+//события на кнопки
+btnNumber.forEach(function (item) {
+    item.addEventListener("click", function() {
+        string.value += item.textContent;
+    });
+});
+
+btnSymbol.forEach(function (item) {
+    item.addEventListener("click", function() {
+        string.value += item.textContent;
+    });
+});
+
+btnOperations.forEach(function (item) {
+    item.addEventListener("click", function() {
+        string.value += item.textContent;
+    });
+});
+btnOperations[5].addEventListener('click', function() {
+    string.value = 0;
+    result = 0;
+});
+
+//запрет ввода лишних символов
+string.onkeypress = function(e) {
+
+    e = e || event;
+
+    let chr = getChar(e);
+    if (chr == null) return;
+    if (chr == '(' || chr == ')' || chr == '*' || chr == '/' || chr == '+' || chr == '-' || chr == '=' || chr == ',') {
+        return true;
+    } else if (chr < '0' || chr > '9') {
+        return false;
+    }
+  };
+
+function getChar(event) {
+    if (event.which != 0 && event.charCode != 0) {
+        if (event.which < 32) return null;
+        return String.fromCharCode(event.which);
+    }
+    return null;
+}
+
+//вычисление
+equals = addEventListener('click', function() {
+    result = eval(string.value);
+    string.value = String(result);
+})
